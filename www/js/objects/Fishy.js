7bc105dfe7b5ea9f5f2dcce439fc93caebbe23e9
Cole Panike <cole.panike@walmart.com>
2017-06-14 18:01:14 -0500

    Do screenwrap rather than world border collision

--- www/js/objects/Fishy.js
+++ www/js/objects/Fishy.js
@@ -1,4 +1,3 @@
-
 var Fishy = function (game) {
     Phaser.Sprite.call(this, game, game.world.randomX, game.world.randomY, 'fishy');
 
@@ -14,28 +13,46 @@ var Fishy = function (game) {
     game.physics.arcade.velocityFromRotation(this.rotation, 100, this.body.velocity);
 
     // Bounce from edges
-    this.body.collideWorldBounds = true;
-    this.body.worldBounce = new Phaser.Point(1,1);
-    // Alter rotation and sprite orientation on collision
-    this.body.onWorldBounds = new Phaser.Signal();
-    this.body.onWorldBounds.add(function (thisFishy, up, down, left, right) {
-        thisFishy.rotation = game.math.angleBetween(0,0,thisFishy.body.velocity.x,thisFishy.body.velocity.y);
-        thisFishy.spriteFlip();
-    });
+    // this.body.collideWorldBounds = true;
+    // this.body.worldBounce = new Phaser.Point(1,1);
+    // // Alter rotation and sprite orientation on collision
+    // this.body.onWorldBounds = new Phaser.Signal();
+    // this.body.onWorldBounds.add(function (thisFishy, up, down, left, right) {
+    //     thisFishy.rotation = game.math.angleBetween(0,0,thisFishy.body.velocity.x,thisFishy.body.velocity.y);
+    //     thisFishy.spriteFlip();
+    // });
 };
 Fishy.prototype = Object.create(Phaser.Sprite.prototype);
 Fishy.prototype.constructor = Fishy;
 
 Fishy.prototype.spriteFlip = function () {
-    if(Math.abs(this.angle) > 90) {
-            // Going rtl
-            if (this.scale.y > 0){
+    if (Math.abs(this.angle) > 90) {
+        // Going rtl
+        if (this.scale.y > 0) {
             this.scale.y *= -1;
-            }
-        } else {
+        }
+    } else {
         // Going ltr
         if (this.scale.y < 0) {
             this.scale.y *= -1;
         }
+    }
+}
+
+Fishy.prototype.update = function () {
+    screenWrap(this);
+}
+
+function screenWrap(sprite) {
+    if (sprite.x < 0 - sprite.width) {
+        sprite.x = sprite.game.world.width + sprite.width;
+    } else if (sprite.x > sprite.game.world.width + sprite.width) {
+        sprite.x = 0 - sprite.width;
+    }
+
+    if (sprite.y < 0 - sprite.width) {
+        sprite.y = sprite.game.world.height + sprite.width;
+    } else if (sprite.y > sprite.game.world.height + sprite.width) {
+        sprite.y = 0 - sprite.width;
     }
 }
